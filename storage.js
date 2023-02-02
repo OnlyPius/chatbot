7503053d241b068e2e29ed9c9c048f751f5d8d45
OnlyPius <118131400+OnlyPius@users.noreply.github.com>
2023-02-02 11:49:47 +0000

    Add files via upload

---
+++ storage.js
@@ -0,0 +1,9 @@
+window.addEventListener("load",()=>{
+    if(localStorage.getItem("data"=== null)){
+      localStorage.setItem("data","elinam")
+
+    }
+    // localStorage.get("data")
+  })
+  console.log("test")
+ 
